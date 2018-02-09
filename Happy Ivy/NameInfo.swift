28aa39f498870b811cdc1bf610dc99f8568c87df
Keke Wu <keke@rl1-1-158-252-edu.int.colorado.edu>
2018-02-09 12:26:52 -0700

    first commit

---
+++ Happy Ivy/NameInfo.swift
@@ -0,0 +1,9 @@
+//
+//  NameInfo.swift
+//  Happy Ivy
+//
+//  Created by Keke Wu on 2/9/18.
+//  Copyright © 2018 Keke Wu. All rights reserved.
+//
+
+import Foundation
---
+++ Happy Ivy/NameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NameViewController.swift
+//  Happy Ivy
+//
+//  Created by Keke Wu on 2/9/18.
+//  Copyright © 2018 Keke Wu. All rights reserved.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
