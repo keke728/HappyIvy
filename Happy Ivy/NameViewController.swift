b666040b1b34037a33062d934b54cfb22917213c
Jake <jjohnson5253@yahoo.com>
2018-02-09 22:25:35 -0700

    integrated working model

--- Happy Ivy/NameViewController.swift
+++ Happy Ivy/NameViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class NameViewController: UIViewController, UITextFieldDelegate {
      @IBOutlet weak var nameBox: UITextField!
@@ -27,15 +28,51 @@ class NameViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: Save
+    
+    /**
+     Saves input string as user name to User entity in Model data model
+     
+     - Parameters:
+     - name: the user name to be saved
+     */
+    func saveName(name: String) {
+        
+        // Instatiate appDelegate and managedContext
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Declare user object to hold the first User data entry
+        var user : NSObject
+        
+        // Initialize an entry in data model.
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: managedContext)!
+        user = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // Set entry value for name
+        user.setValue(name, forKeyPath: "name")
+        
+        // Perform built in save function
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
-    */
 
 }
+
+// MARK:- ---> UITextFieldDelegate
+extension NameViewController {
+    
+    func textFieldShouldReturn(_ nameBox: UITextField) -> Bool {
+        // called when 'return' key pressed. return NO to ignore.
+        saveName(name: nameBox.text!)
+        performSegue(withIdentifier: "gotoPlant", sender: NameViewController.self)
+        return true
+    }
+    
+}
+// MARK: UITextFieldDelegate <---
