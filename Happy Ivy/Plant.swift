fb1c94187f6740e11d9fc56b10548c63bd2fa0ce
Lucas Dachman <lucas.dachman@gmail.com>
2018-02-10 12:07:59 -0700

    Added Plant.swift which is a data model for the plant. The Plant class contains an array to refer to the list of plants

--- Happy Ivy/Plant.swift
+++ Happy Ivy/Plant.swift
@@ -9,5 +9,55 @@
 import Foundation
 
 class Plant {
+    
+    static var plantList: [Plant] = []
+    
+    // MARK: Public Members
     var name: String?
+    var species: String?
+    var description: String?
+    
+    // MARK: Private Members
+    private var air: Int = 10
+    private var water: Int = 10
+    private var sun: Int = 10
+    private var love: Int = 10
+    
+    // MARK: Initializers
+    init(name: String) {
+        self.name = name
+    }
+    init(name: String, species: String, description: String) {
+        self.name = name
+        self.species = species
+        self.description = description
+    }
+    
+    // MARK: Public Methods
+    func increaseAir() {
+        air += 5
+    }
+    func decreaseAir() {
+        air -= 5
+    }
+    func increaseWater() {
+        water += 5
+    }
+    func decreaseWater() {
+        water -= 5
+    }
+    func increaseSun() {
+        sun += 5
+    }
+    func decreaseSun() {
+        sun -= 5
+    }
+    func increaseLove() {
+        love += 5
+    }
+    func decreaseLove() {
+        love -= 5
+    }
+    
+    
 }
